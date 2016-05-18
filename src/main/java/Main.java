9e3c00b739cbdc496a563651c4150bf6818d5e2c
diego.silva@apuntesdejava.com <>
2016-05-18 16:43:56 -0500

    micro-payara con war

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,38 @@
+
 import fish.payara.micro.BootstrapException;
 import fish.payara.micro.PayaraMicro;
+import java.io.File;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.glassfish.embeddable.GlassFish;
+import org.glassfish.embeddable.GlassFishException;
+import org.glassfish.embeddable.GlassFishProperties;
+import org.glassfish.embeddable.GlassFishRuntime;
 
 public class Main {
 
-  public static void main(String[] args) throws BootstrapException {
-      PayaraMicro.getInstance()
-              .setHttpPort(Integer.parseInt(System.getenv("PORT")))
-              .addDeployment("payara-sample-web-1.0-SNAPSHOT.war")
-              .bootStrap();
+    private static final Logger LOG = Logger.getLogger(Main.class.getName());
 
-  }
+    public static void main(String[] args) throws BootstrapException, GlassFishException {
+        int puerto = 10000;
+        String sPuerto = System.getenv("PORT");
+        if (sPuerto != null && !sPuerto.isEmpty()) {
+            puerto = Integer.parseInt(sPuerto);
+        }
+        File file = new File("payara-sample-web.war");
+        LOG.log(Level.INFO, "war path:{0}", file.getAbsolutePath());
+        LOG.log(Level.INFO, "Existe:{0}", file.exists());
+        PayaraMicro micro = PayaraMicro.getInstance();
+        micro.setHttpPort(puerto)
+                .addDeploymentFile(file)
+                .bootStrap();
+        /*GlassFishRuntime runtime = GlassFishRuntime.bootstrap();
+        GlassFishProperties props = new GlassFishProperties();
+        props.setPort("http-listener", puerto);
+        GlassFish gf = runtime.newGlassFish(props);
+        gf.start();
+        gf.getDeployer().deploy(file);*/
+
+    }
 
 }
